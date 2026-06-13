db4931a1aa8927f91e12701fbc6a2c6bd1cee7df
alfardan <alfardanarfin500@gmail.com>
2026-06-13 14:44:13 +0530

    Add new components and assets for the React application
    - Introduced About, Contact, Footer, Github, Header, Home, and User components.
    - Implemented routing using React Router with a layout structure.
    - Added a GitHub API integration to fetch user followers.
    - Included a new SVG asset for Vite branding.
    - Configured Tailwind CSS with Vite for styling.
    - Created index.css for global styles.

---
+++ react-router/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import Home from './components/Home/Home.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   
+   </>
+  )
+}
+
+export default App
